16436d4b032c5a72683c5eb316646e5ec9c4b2f6
Corentin ROBERT <Corentin.Robert@outlook.com>
2020-01-14 17:59:55 +0100

    Ajoutez des fichiers projet.

---
+++ Client.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banque
+{
+    public class Client
+    {
+        private float argent;
+        private string nom;
+
+        public float Argent { get => argent; set => argent = value; }
+        public string Nom { get => nom; set => nom = value; }
+
+        public Client()
+        {
+            Nom = "Dupont";
+            Argent = 0f;
+        }
+        public Client(float nbArgent,string nom)
+        {
+            Argent = nbArgent;
+            Nom = nom;
+        }
+        public void Virement_autre_client(Client client,float somme)
+        {
+            if (this.Argent < somme)
+            {
+                throw new Exception("Virement impossible, compte bancaire pas asser fournit");
+            }
+            if (somme < 0)
+            {
+                throw new Exception("Virement impossible avec une somme negative");
+            }
+            else
+            {
+                client.Recevoir_argent(somme);
+                this.Perdre_argent(somme);
+            }
+        }
+        public void Recevoir_argent(float somme)
+        {
+            if (somme < 0)
+            {
+                throw new Exception("Impossible de recevoir de l'argent négatif");
+            }
+            else
+                Argent += somme;
+        }
+        public void Perdre_argent(float somme)
+        {
+            if (somme < 0)
+            {
+                throw new Exception("Impossible de perdre de l'argent négatif");
+            }
+            if(Argent-somme < 0)
+            {
+                throw new Exception("On ne peut avoir de compte en banque negatif");
+            }
+            else
+            {
+                Argent -= somme;
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            return "Le client "+Nom+" détient :"+Argent+" Euros";
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+            else
+            {
+                Client p = (Client)obj;
+                return (p.Nom == Nom) && (p.Argent == Argent);
+            }
+            
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(argent, nom, Argent, Nom);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Banque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client coco = new Client(1500f, "Coco");
+            Client nono = new Client(10, "Nono");
+            Client toto = new Client();
+            Client tata = new Client();
+            Console.WriteLine(toto.Equals(tata));
+        }
+    }
+}
